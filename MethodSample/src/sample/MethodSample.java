d907a0daa5228f78825bee79e51325e0cbec2335
RyoUdaka(宇髙凌生) <RyoUdaka()@Dirbato-1401x>
2024-04-08 16:31:55 +0900

    演習開始

--- MethodSample/src/sample/MethodSample.java
+++ MethodSample/src/sample/MethodSample.java
@@ -3,8 +3,14 @@ package sample;
 public class MethodSample {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+        int number1 = 100;
+        int number2 = 10;
 
+        sumMethod1(number1, number2);
     }
 
+    public static void sumMethod1(int num1, int num2) {
+        int result = num1 + num2;
+        System.out.println("sumMethod1の結果は" + result);
+    }
 }
